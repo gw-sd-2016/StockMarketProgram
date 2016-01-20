c7ced6ed221b3abffb7737a232924d39e7706bb7
dougdellolio <doug.dellolio@gmail.com>
2016-01-19 19:05:11 -0500

    [Test] MainFrame

---
+++ src/tests/MainFrameTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import static org.junit.Assert.*;
+import java.io.IOException;
+import java.text.ParseException;
+import org.junit.Test;
+import main.MainFrame;
+
+public class MainFrameTest {
+
+	@Test
+	public void returnHeadLineYearTest() throws ParseException, IOException {
+		int result = MainFrame.returnHeadLineYear("1994-08-15");
+		assertEquals(result, 1994);
+	}
+	
+	@Test
+	public void returnHeadLineMonthTest() throws ParseException, IOException {
+		int result = MainFrame.returnHeadLineMonth("1994-08-15");
+		assertEquals(result, 8);
+	}
+	
+	@Test
+	public void returnHeadLineDayTest() throws ParseException, IOException {
+		int result = MainFrame.returnHeadLineDay("1994-08-15");
+		assertEquals(result, 15);
+	}
+}
