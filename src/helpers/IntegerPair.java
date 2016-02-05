d92e0ecc29f7b12eb6beba9fe3e52be45bc6853e
dougdellolio <doug.dellolio@gmail.com>
2016-02-04 19:57:39 -0500

    [Feature] Search Pane

---
+++ src/helpers/IntegerPair.java
@@ -0,0 +1,20 @@
+package helpers;
+
+public class IntegerPair {
+	int startingIndex;
+	int endingIndex;
+
+	public IntegerPair(int start, int finish) {
+		startingIndex = start;
+		endingIndex = finish;
+	}
+
+	public int returnStart() {
+		return startingIndex;
+	}
+
+	public int returnEnd() {
+		return endingIndex;
+	}
+
+}
